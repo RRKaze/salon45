41b69d04dd3a16785467b0500b9db7c388cf1c17
Xinyi Wang <36870943+RRKaze@users.noreply.github.com>
2025-10-15 22:48:11 -0400

    initial commit

---
+++ backend/src/routes/userRoutes.ts
@@ -0,0 +1,13 @@
+import { Router, type Request, type Response } from "express";
+const router = Router();
+
+router.get("/", (req: Request, res: Response) => {
+  res.json({ message: "List of users" });
+});
+
+router.post("/", (req: Request, res: Response) => {
+  const { name } = req.body;
+  res.json({ message: `User ${name} created!` });
+});
+
+export default router;
---
+++ backend/src/server.ts
@@ -0,0 +1,28 @@
+import express, { type Application, type Request, type Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+// import userRoutes from "./routes/userRoutes";
+import userRoutes from "./routes/userRoutes.ts";
+
+
+
+dotenv.config();
+
+const app: Application = express();
+const PORT = process.env['PORT'] || 3000;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Example route
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello from TypeScript backend!");
+});
+
+app.use("/api/users", userRoutes);
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
