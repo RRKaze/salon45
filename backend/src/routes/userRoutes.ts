482097ebf24a6c6d38c8f1f0dd720b060d856b35
Xinyi Wang <36870943+RRKaze@users.noreply.github.com>
2025-10-18 00:24:08 -0400

    added get user

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,8 +1,18 @@
 import { Router, type Request, type Response } from "express";
+import { MongoClient } from "mongodb";
 const router = Router();
 
-router.get("/", (_req: Request, res: Response) => {
-  res.json({ message: "List of users" });
+router.get("/", async (_req: Request, res: Response) => {
+    const mongoClient = await GetMongoClient(process.env);
+    await mongoClient.connect();
+    const salonDB = mongoClient.db('salon');
+    const userCollection = salonDB.collection('user');
+    const users = userCollection.find({ });
+    const result = [];
+    while (await users.hasNext()) {
+      result.push(await users.next());
+    };
+    res.json(result);
 });
 
 router.post("/", (req: Request, res: Response) => {
@@ -10,4 +20,14 @@ router.post("/", (req: Request, res: Response) => {
   res.json({ message: `User ${name} created!` });
 });
 
+async function GetMongoClient(env: NodeJS.ProcessEnv) {
+  const uri = env['MongoConnectionString'];
+  if (uri == undefined) {
+    throw "Mongo connection string is undefined";
+  }
+  return new MongoClient(uri);
+}
+
+
 export default router;
+
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,6 @@ import express, { type Application, type Request, type Response } from "express"
 import dotenv from "dotenv";
 import cors from "cors";
 import userRoutes from "./routes/userRoutes.js";
-import { MongoClient } from "mongodb";
 
 dotenv.config();
 
@@ -20,26 +19,7 @@ app.get("/", (_req: Request, res: Response) => {
 
 app.use("/api/users", userRoutes);
 
-(async () => {
-  const mongoClient = await GetMongoClient(process.env);
-  await mongoClient.connect();
-  const testDb = mongoClient.db('testDb');
-  const moviesCollection = testDb.collection('movies');
-  const movies = moviesCollection.find({ });
-  while (await movies.hasNext()) {
-    console.log(await movies.next());
-  }
-})();
-
 // Start server
 app.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
-
-async function GetMongoClient(env: NodeJS.ProcessEnv) {
-    const uri = env['MongoConnectionString'];
-    if (uri == undefined) {
-      throw "Mongo connection string is undefined";
-    }
-    return new MongoClient(uri);
-}
