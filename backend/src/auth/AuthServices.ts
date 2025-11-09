2f6e71a018f30751e6dad7d51da72442e9a1c5d8
Janus <bruno.sotelo@proton.me>
2025-11-09 16:07:06 -0300

    Return json for login response

--- backend/src/auth/AuthServices.ts
+++ backend/src/auth/AuthServices.ts
@@ -3,6 +3,7 @@ import { type DependencyContainer } from "tsyringe";
 import passport from "passport";
 import { LocalStrategy } from "./services/LocalStrategy.ts";
 import { SessionService } from "./services/SessionService.ts";
+import type { User } from "../users/models/User.ts";
 
 export class AuthServices {
   public register(container: DependencyContainer): Router {
@@ -15,11 +16,22 @@ export class AuthServices {
     passport.serializeUser(sessionService.Serialize);
     passport.deserializeUser(sessionService.Deserialize);
 
-    router.post(
-      "/login",
-      passport.authenticate("local", {
-        failureMessage: true,
-      }),
+    router.post("/login", (req, res, next) =>
+      passport.authenticate("local", (err: Error, usr: User) => {
+        if (err) {
+          return next(err);
+        } else if (!usr) {
+          return res
+            .status(401)
+            .json({ success: false, error: "USER_NOT_FOUND" });
+        }
+        return req.logIn(usr, (err) => {
+          if (err) {
+            return next(err);
+          }
+          return res.json();
+        });
+      })(req, res, next),
     );
 
     return router;
